f52c5b3befb89c29caaa05bd0cb8d299402e72b5
tugranDemirel <demireltugran66@gmail.com>
2021-12-29 21:59:29 +0300

    veritabanı urun ekleme, guncelleme kontrolleri

--- app/Http/Controllers/Backend/Product/ProductController.php
+++ app/Http/Controllers/Backend/Product/ProductController.php
@@ -8,71 +8,120 @@ use App\Models\Menu;
 use App\Models\Product;
 use Delight\Random\Random;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
+    // URUN LISTESI
+
+    public function list()
+    {
+        if (Auth::check())
+        {
+            $products = Product::all();
+            return view('Backend.Products.product_list', compact('products'));
+        }
+    }
+
+    // CRAETA PAGE ITEM LIST
+
     public function show()
     {
-        $brands = Brand::all();
-        $menus = Menu::where('status', 1)->get();
-        return view('Backend.Products.product_create', compact('brands', 'menus'));
+        if (Auth::check())
+        {
+            $brands = Brand::all();
+            $menus = Menu::where('status', 1)->get();
+            return view('Backend.Products.product_create', compact('brands', 'menus'));
+        }
+        else
+        {
+            toastError('Geçersiz işlem.');
+            return redirect()->back();
+        }
     }
 
     public function create(Request $request)
     {
 
-        $product = new Product;
-
-        $product->title = $request->title;
-        $product->slug = Str::slug($request->title).'-'.Random::alphanumericHumanString(10);
-        $product->about = $request->about;
-        $product->description = $request->description;
-        $product->keywords = $request->keywords;
-        $product->hash = Random::alphaLowercaseHumanString(28);
-        $product->price = $request->price;
-        $product->discount = $request->discount;
-        $product->stock = 10;
-        $product->menu_id = $request->menu;
-        $product->brand_id = $request->brand;
-        $product->code = $request->code;
-        $product->status = 1;
-        $product->hit = 0;
-
-        if ($request->hasFile('images'))
+        if (Auth::check())
         {
-            $imageName = Str::slug($request->title, '-') . '-' . rand(1200, 199999) . '.' . $request->images->extension(); // getClientOriginalExtension()
-            $request->images->move(public_path('uploads/products/'), $imageName);
-            $product->images = 'products'.$imageName;
+            $isHash = Random::alphaLowercaseHumanString(28);
+            $hash = Product::where('hash', $isHash)->first();
+            if ($hash)
+            {
+                toastError('Bir hata oluştu. Lütfen tekrar deneyiniz.');
+                return redirect()->back();
+            }
+            else
+            {
+                $product = new Product;
+
+                $product->title = $request->title;
+                $product->slug = Str::slug($request->title).'-'.Random::alphanumericHumanString(10);
+                $product->about = $request->about;
+                $product->description = $request->description;
+                $product->keywords = $request->keywords;
+                $product->hash = $isHash;
+                $product->price = $request->price;
+                $product->discount = $request->discount;
+                $product->stock = 10;
+                $product->menu_id = $request->menu;
+                $product->brand_id = $request->brand;
+                $product->code = $request->code;
+                $product->status = 1;
+                $product->hit = 0;
+
+                if ($request->hasFile('images'))
+                {
+                    $imageName = Str::slug($request->title, '-') . '-' . rand(1200, 199999) . '.' . $request->images->extension(); // getClientOriginalExtension()
+                    $request->images->move(public_path('uploads/products/'), $imageName);
+                    $product->images = 'products'.$imageName;
+                }
+                $product->save();
+                toastSuccess('Ekleme işlemi başarılı bir şekilde gerçekleştirilmiştir.');
+                return redirect()->route('admin.index');
+            }
         }
-        $product->save();
-        toastSuccess('Ekleme işlemi başarılı bir şekilde gerçekleştirilmiştir.');
-        return redirect()->route('admin.index');
+        else
+        {
+            toastError('Geçersiz işlem.');
+            return redirect()->back();
+        }
     }
 
-    // URUN LISTESI
-
-    public function list()
-    {
-        $products = Product::all();
-        return view('Backend.Products.product_list', compact('products'));
-    }
 
     // URUN EDIT
 
     public function edit($hash)
     {
-        $brands = Brand::all();
-        $menus = Menu::where('status', 1)->get();
-        $product = Product::where('hash', $hash)->firstOrFail();
-        return view('Backend.Products.product_edit', compact('brands', 'menus', 'product'));
+        if (Auth::check())
+        {
+            $brands = Brand::all();
+            $menus = Menu::where('status', 1)->get();
+            $product = Product::where('hash', $hash)->firstOrFail();
+            return view('Backend.Products.product_edit', compact('brands', 'menus', 'product'));
+        }
+        else
+        {
+            toastError('Geçersiz işlem.');
+            return redirect()->back();
+        }
     }
 
     // URUN DESTROY
 
     public function destroy()
     {
-        toastSuccess('Başarılı bir şekilde silme işlemi gerçekleştirilmiştir');
-        return redirect()->route('product.list');
+        if (Auth::check())
+        {
+            toastSuccess('Başarılı bir şekilde silme işlemi gerçekleştirilmiştir');
+            return redirect()->route('product.list');
+        }
+        else
+        {
+            toastError('Geçersiz işlem.');
+            return redirect()->back();
+        }
     }
 }
