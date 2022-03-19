5ad1f13b2709d3af6e0365d7e39cacc5f29ce153
tugranDemirel <demireltugran66@gmail.com>
2022-03-19 03:22:28 +0300

    Frontend template parçalanması. welcome.blade.php silinmesi

---
+++ resources/views/Frontend/layouts/banner.blade.php
@@ -0,0 +1,33 @@
+
+<section class="banner-part">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="banner-content"
+                     style="background: url({{ asset('Frontend/images/shop/banner/clothes/banner.jpg') }}) no-repeat center; background-size: cover;">
+                    <h1>get your clothe items quickly.</h1>
+                    <p>We are always ready to deliver product to your doorstep every day</p>
+                    <form class="banner-form">
+                        <div class="select-option"><i class="icon icofont-jacket"></i><span class="text">clothes</span>
+                            <ul class="option-list">
+                                <li><a href="https://mironmahmud.com/ecomart/assets/ltr/html/grocery/index.html"><i
+                                            class="icofont-grocery"></i><span>grocery</span></a></li>
+                                <li><a href="https://mironmahmud.com/ecomart/assets/ltr/html/bakery/index.html"><i
+                                            class="icofont-cup-cake"></i><span>bakery</span></a></li>
+                                <li><a href="index.html"><i class="icofont-jacket"></i><span>clothes</span></a></li>
+                                <li><a href="https://mironmahmud.com/ecomart/assets/ltr/html/gadgets/index.html"><i
+                                            class="icofont-camera"></i><span>gadgets</span></a></li>
+                                <li><a href="https://mironmahmud.com/ecomart/assets/ltr/html/furniture/index.html"><i
+                                            class="icofont-chair"></i><span>furniture</span></a></li>
+                                <li><a href="https://mironmahmud.com/ecomart/assets/ltr/html/cosmetics/index.html"><i
+                                            class="icofont-paint"></i><span>cosmetics</span></a></li>
+                            </ul>
+                        </div>
+                        <input type="text" placeholder="Search anything...">
+                        <button type="submit"><i class="icofont-ui-search"></i><span>search</span></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
